2756e30538baa2ac2be8900b3e9577734311214b
Bibi <84073518+xBibie@users.noreply.github.com>
2021-06-30 19:36:33 +0200

    Update pizza.js

--- pizza.js
+++ pizza.js
@@ -1,4 +1,4 @@
-const makePizza () {
+const makePizza =function() {
     console.log ('take dough, make dough into pancake shape');
     console.log ('Take tomato sauce and spread over dough');
     console.log ('add all your favourite toppings');
@@ -10,7 +10,7 @@ const makePizza () {
 
 
 
-const makeSushi () {
+const makeSushi =function() {
     console.log ('sushi is nasty, what u need? fish, rice and seawees I guess');
     console.log ('Cook rice');
     console.log ('no cook fish, u eat raw apperently');
