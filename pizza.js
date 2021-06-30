d8c186cee12490bd8384922cd7396c63170c7cb6
Bibi <84073518+xBibie@users.noreply.github.com>
2021-06-30 19:34:56 +0200

    Add files via upload

---
+++ pizza.js
@@ -0,0 +1,28 @@
+const makePizza () {
+    console.log ('take dough, make dough into pancake shape');
+    console.log ('Take tomato sauce and spread over dough');
+    console.log ('add all your favourite toppings');
+    console.log ('like pepper, pepperoni, mushroom, ham, etc.');
+    console.log ('put a shit load of cheese on top');
+    console.log ('bake for 10 mins in 180 degree celcius');
+    console.log ('Buen appetito');
+}
+
+
+
+const makeSushi () {
+    console.log ('sushi is nasty, what u need? fish, rice and seawees I guess');
+    console.log ('Cook rice');
+    console.log ('no cook fish, u eat raw apperently');
+    console.log ('make rolls with seaweed leaves rice and fishy ');
+    console.log ('they will get fucked up');
+    console.log ('throw away, eat pizza instead');
+    console.log ('Buen appetito');
+}
+
+makeSushi ();
+makeSushi ();
+makeSushi ();
+makePizza ();
+makePizza ();
+
